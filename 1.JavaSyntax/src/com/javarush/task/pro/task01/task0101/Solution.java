0b80fb41df4b1701870d5c812ca19b5dddea0819
Inter2006 <9539021231@mail.ru>
2023-02-18 18:04:09 +0300

    add solution java syntax task 0101

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task01/task0101/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task01.task0101;
+
+/* 
+Первая программа
+*/
+
+public class Solution { 
+
+    public static void main(String[] args) {
+    System.out.print("Диего ");
+    System.out.print("+ ");
+    System.out.print("Амиго ");
+    System.out.print("= ");
+    System.out.print("Друзья");//outнапишите тут ваш код
+    }
+}
