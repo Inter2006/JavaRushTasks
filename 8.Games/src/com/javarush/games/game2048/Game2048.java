ad05601c2793e0162b4c1bcb0b3f08d889064681
Inter2006 <9539021231@mail.ru>
2023-02-25 18:06:48 +0300

    add solution java game 2048 1/18

---
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -0,0 +1,7 @@
+package com.javarush.games.game2048;
+
+import com.javarush.engine.cell.*;
+
+public class Game2048 extends Game {
+}
+//--module-path "C:\MyProjects\JavaRushTasks\lib\javafx-sdk-17.0.2\lib" --add-modules javafx.controls,javafx.fxml
