fd424b9d90de8280ee4cb0d6b60e3df60140627b
Inter2006 <9539021231@mail.ru>
2023-03-04 18:03:59 +0300

    add solution java game 2048 6/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -24,7 +24,7 @@ public class Game2048 extends Game {
     private void drawScene() {
         for (int y = 0; y < gameField.length; y++) {
             for (int x = 0; x < gameField[y].length; x++) {
-                setCellColor(x, y, Color.BLUE);
+                setCellColoredNumber(x, y, gameField[y][x]);
             }
         }
     }
@@ -42,7 +42,43 @@ public class Game2048 extends Game {
         }else{
             createNewNumber();
         }
-
+    }
+    private  Color getColorByValue(int value){
+        if(value == 0){
+            return Color.WHITE;
+        } else if (value == 2) {
+            return Color.AZURE;
+        } else if (value == 4) {
+            return Color.AQUA;
+        } else if (value == 8) {
+            return Color.BEIGE;
+        } else if (value == 16) {
+            return Color.ALICEBLUE;
+        } else if (value == 32) {
+            return Color.RED;
+        } else if (value == 64) {
+            return Color.BISQUE;
+        } else if (value == 128) {
+            return Color.CORAL;
+        } else if (value == 256) {
+            return Color.YELLOW;
+        } else if (value == 512) {
+            return Color.PINK;
+        } else if (value == 1024) {
+            return Color.FUCHSIA;
+        } else if (value == 2048) {
+            return Color.LIME;
+        }else{
+            return Color.NONE;
+        }
+    }
+    private void setCellColoredNumber(int x, int y, int value){
+        Color color = getColorByValue(value);
+        String string = "";
+        if(value > 0){
+            string = value + "";
+        }
+        setCellValueEx(x, y, color, string);
     }
 }
 //--module-path "C:\MyProjects\JavaRushTasks\lib\javafx-sdk-17.0.2\lib" --add-modules javafx.controls,javafx.fxml
