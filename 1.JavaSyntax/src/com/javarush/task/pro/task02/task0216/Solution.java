35b76578db56a29f9df16fb37147abdd509e1687
Inter2006 <9539021231@mail.ru>
2023-03-11 18:51:01 +0300

    task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Алекс";
+        String date = "сегодня";
+        String time = "в 19:00";
+        String adjective = "превосходный";
+        String noun = "генератор";
+        String verb = "помогает";//напишите тут ваш код
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}
