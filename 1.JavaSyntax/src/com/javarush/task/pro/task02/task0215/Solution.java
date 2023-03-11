a02e1f4fcdb86104a8bc8f5d789f8edcb8772526
Inter2006 <9539021231@mail.ru>
2023-03-11 18:50:49 +0300

    task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/*
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner console = new Scanner(System.in);
+        int a1 = console.nextInt();
+
+        int a2 = console.nextInt();
+
+        int a3 = console.nextInt();
+
+        System.out.println((a1 + a2 + a3) / 3);
+    }
+}
+        //напишите тут ваш код
