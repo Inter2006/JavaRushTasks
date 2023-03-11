89b4dca014b172d7bbdd3227a33cbd44076f4054
Inter2006 <9539021231@mail.ru>
2023-03-11 18:51:37 +0300

    task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 125;
+        int age = 56;
+
+        String documents = "Документы";
+
+        String behavior = "Преступник"; //напишите тут ваш код
+    }
+}
