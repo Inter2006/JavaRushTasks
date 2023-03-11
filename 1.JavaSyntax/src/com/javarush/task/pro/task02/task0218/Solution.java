f2d6d66b68b3912024707802b7823b78ec35b32b
Inter2006 <9539021231@mail.ru>
2023-03-11 18:51:25 +0300

    task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0218/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0218;
+
+import java.util.Scanner;
+
+/* 
+Ручное управление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int s = console.nextInt();
+        int g = console.nextInt();
+        int h = console.nextInt();
+        System.out.println(s * g * h);//напишите тут ваш код
+    }
+}
