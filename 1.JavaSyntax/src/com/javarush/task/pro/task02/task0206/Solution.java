012978993ad290246e1fe23a6a26aa91295756e3
Inter2006 <9539021231@mail.ru>
2023-03-11 18:48:16 +0300

    task completed

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "сон", phrase = "был очень", line = "хорошим", text = ". Я выспался , я бодр и здоров. ";//Ладно кто читает это , это всё не правда. я почти что не сплю хехе.
+
+    }
+}
