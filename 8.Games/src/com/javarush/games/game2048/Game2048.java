f48b937aa19032e3daced639273ac64bfd02cda1
Inter2006 <9539021231@mail.ru>
2023-04-01 18:01:00 +0300

    add solution java game 2048 11/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -137,7 +137,7 @@ public class Game2048 extends Game {
                 isMotion = true;
             }
         }
-        if(isMotion){
+        if (isMotion) {
             createNewNumber();
         }
     }
@@ -152,6 +152,16 @@ public class Game2048 extends Game {
 
     private void moveDown() {
 
+    }
+
+    private void rotateClockwise() {
+        int[][] newGameField = new int[SIDE][SIDE];
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                newGameField[j][SIDE - 1 - i] = gameField[i][j];
+            }
+        }
+        gameField = newGameField;
     }
 }
 //--module-path "C:\MyProjects\JavaRushTasks\lib\javafx-sdk-17.0.2\lib" --add-modules javafx.controls,javafx.fxml
