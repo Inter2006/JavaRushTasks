ac7d81a3ca4b04c5d59c9e5cef62538371fc20b6
Inter2006 <9539021231@mail.ru>
2023-04-28 22:29:41 +0300

    add solution  java syntax task 0620

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0620/Mrakobes.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0620/Mrakobes.java
@@ -1,11 +1,14 @@
 package com.javarush.task.pro.task06.task0620;
 
-public class Mrakobes {
-    private static boolean isAlive = true;
+    public class Mrakobes {
+        private static boolean isAlive = true;
 
-    //напишите тут ваш код
+        public static void setIsAlive(boolean isAliveParam) {
+            isAlive = isAliveParam;
+        }
 
-    private static void printState() {
-        System.out.println("Амиго" + (isAlive ? " НЕ " : " ") + "победил Мракобеса Безумного");
+        public static void printState() {
+            System.out.println("Амиго" + (isAlive ? " НЕ " : " ") + "победил Мракобеса Безумного");
+        }
     }
-}
+
--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0620/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0620/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.pro.task06.task0620;
 
 public class Solution {
     public static void main(String[] args) {
-//        Mrakobes.isAlive = false;
-//        Mrakobes.printState();
+        Mrakobes.setIsAlive(false);
+        Mrakobes.printState();
     }
 }
