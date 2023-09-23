97eceaec59bd206cc0eea98b869d63cbf6d3db23
Inter_Love <9539021231@mail.ru>
2023-09-23 16:14:31 +0300

    Java Core Tasks сделаны

--- 1.JavaSyntax/src/test/Main.java
+++ 1.JavaSyntax/src/test/Main.java
@@ -1,13 +1,23 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args){
-        Animal animal = new Animal();
-        // animal.sayHello();
-        Cat cat = new Cat("Шотландская-вислаухая","Дуня",3 );
-        // cat.sayHello();
-        Animal myAnimal = cat;
-        myAnimal.sayHello();
+    public static void main(String[] args) {
+        try {
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            String fileName = console.readLine();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while (reader.ready()) {
+                String string = reader.readLine();
+                System.out.println(string);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
